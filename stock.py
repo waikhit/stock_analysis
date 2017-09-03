8e09c51f95e4b1ad72319d514a6da9b6b0d750a7
waikhit <waikhit@dlicsar.com>
2017-09-03 22:02:03 +0800

    added csv and changed the data reader by installing datareader with regular pip

--- stock.py
+++ stock.py
@@ -3,11 +3,16 @@ import matplotlib.pyplot as plt
 from matplotlib import style
 import pandas as pd
 import pandas_datareader.data as web
-
+import numpy as np
 
 style.use('ggplot')
-start = dt.datetime(2000, 1, 1)
-end = dt.datetime(2016, 12, 31)
+start=dt.datetime(2000,1,1)
+end=dt.datetime(2016,12,31)
+df= web.DataReader('TSLA', 'google', start, end)
 
-df = web.Datareader('TSLA', 'yahoo', start, end)
+df.to_csv('tsla.csv')
+#df = pd.read_csv('tsla.csv')
 print(df.head())
+
+df.plot()
+plt.show()
