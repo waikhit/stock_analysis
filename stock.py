4ab91d6e3a0bde97a33e2e09fa42a24be322c313
waikhit <waikhit@dlicsar.com>
2017-09-04 20:40:19 +0800

    completed part 3

--- stock.py
+++ stock.py
@@ -19,3 +19,10 @@ df.dropna(inplace=True)
 print(df.head())
 
 ax1 = plt.subplot2grid((6,1),(0,0), rowspan = 5, colspan = 1)
+ax2 = plt.subplot2grid((6,1),(0,0), rowspan = 5, colspan = 1, sharex = ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
+plt.show
