b1a8ce375d929dcff5355223e204d2325a95926a
waikhit <waikhit@dlicsar.com>
2017-09-04 20:36:44 +0800

    trying yahoo

--- stock.py
+++ stock.py
@@ -8,11 +8,14 @@ import numpy as np
 style.use('ggplot')
 start=dt.datetime(2000,1,1)
 end=dt.datetime(2016,12,31)
-df= web.DataReader('TSLA', 'google', start, end)
 
-df.to_csv('tsla.csv')
-#df = pd.read_csv('tsla.csv')
+
+
+df= web.DataReader('TSLA', 'yahoo', start, end)
+
+df['100ma'] = df['Adj Close'].rolling(window = 100,min_periods = 0).mean()
+df.dropna(inplace=True)
+
 print(df.head())
 
-df.plot()
-plt.show()
+ax1 = plt.subplot2grid((6,1),(0,0), rowspan = 5, colspan = 1)
