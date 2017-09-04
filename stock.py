149e6378b546c5bf0b6f34175998c3c644045e45
waikhit <waikhit@dlicsar.com>
2017-09-04 20:43:56 +0800

    adjusted rowspan

--- stock.py
+++ stock.py
@@ -8,21 +8,16 @@ import numpy as np
 style.use('ggplot')
 start=dt.datetime(2000,1,1)
 end=dt.datetime(2016,12,31)
-
-
-
 df= web.DataReader('TSLA', 'yahoo', start, end)
 
-df['100ma'] = df['Adj Close'].rolling(window = 100,min_periods = 0).mean()
-df.dropna(inplace=True)
-
+df['100ma'] = df['Adj Close'].rolling(window = 100, min_periods = 0).mean()
 print(df.head())
 
 ax1 = plt.subplot2grid((6,1),(0,0), rowspan = 5, colspan = 1)
-ax2 = plt.subplot2grid((6,1),(0,0), rowspan = 5, colspan = 1, sharex = ax1)
+ax2 = plt.subplot2grid((6,1),(5,0), rowspan = 5, colspan = 1, sharex = ax1)
 
 ax1.plot(df.index, df['Adj Close'])
 ax1.plot(df.index, df['100ma'])
 ax2.bar(df.index, df['Volume'])
 
-plt.show
+plt.show()
