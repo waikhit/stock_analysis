05168c86e9505b1953c749a1569767d7dd4fca0b
waikhit <waikhit@dlicsar.com>
2017-09-05 21:16:44 +0800

    tutorial 4

---
+++ stock_tut3.py
@@ -0,0 +1,23 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+import numpy as np
+
+style.use('ggplot')
+start=dt.datetime(2000,1,1)
+end=dt.datetime(2016,12,31)
+df= web.DataReader('TSLA', 'yahoo', start, end)
+
+df['100ma'] = df['Adj Close'].rolling(window = 100, min_periods = 0).mean()
+print(df.head())
+
+ax1 = plt.subplot2grid((6,1),(0,0), rowspan = 5, colspan = 1)
+ax2 = plt.subplot2grid((6,1),(5,0), rowspan = 5, colspan = 1, sharex = ax1)
+
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
+plt.show()
---
+++ stock_tut4.py
@@ -0,0 +1,28 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.finance import candlestick_ohlc
+import matplotlib.dates as mdates
+from matplotlib import style
+import pandas_datareader.data as web
+import numpy as np
+
+style.use('ggplot')
+start=dt.datetime(2000,1,1)
+end=dt.datetime(2016,12,31)
+df= web.DataReader('TSLA', 'yahoo', start, end)
+
+df_ohlc = df['Adj Close'].resample('10D').ohlc()
+df_volume = df['Volume'].resample('10D').sum()
+df_ohlc.reset_index(inplace=True)
+print(df_ohlc.head())
+
+df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
+
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan = 5, colspan =1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan = 1, colspan = 1, sharex=ax1)
+ax1.xaxis_date()
+
+candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
+ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values(), 0)
+plt.show()
